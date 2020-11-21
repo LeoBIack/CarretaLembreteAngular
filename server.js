e2d153ad1caabd0da340c20e33a281473da17f46
Leonardo Preto <leonardofpreto@gmail.com>
2020-11-21 20:59:13 -0300

    ITS WORKING

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const db = require("./app/models");
 const Role = db.role;
 
 db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  .connect('mongodb+srv://user:12345@cluster0.1zrgk.mongodb.net/app-mean?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
